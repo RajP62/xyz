4d4e97dc3826aacbf45867a308c8f5b4b97342c1
RajP62 <rajesh4151999@gmail.com>
2022-05-07 21:39:18 +0530

    new

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-document.body.append(document.createElement("h1").innerText = "Hello");
+const data = require("../db.json");
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.get("/data",async(req,res)=>{
+    try{
+        res.send(data);
+    }
+    catch(e){
+        res.send(e);
+    }
+});
+
+app.listen(4000,async()=>{
+    console.log("Server is running on port 4000");
+})
+
+
+
